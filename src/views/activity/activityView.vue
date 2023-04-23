7e7785df7ae2e65a7204a8e1ac5e2dbfc5a4ca7d
lizhanpeng <885387491@qq.com>
2023-04-23 16:56:40 +0800

    Update code

--- src/views/activity/activityView.vue
+++ src/views/activity/activityView.vue
@@ -1,15 +1,378 @@
 <template>
-    <div>
-        activity
+    <div class="activity wrap">
+        <div class="describe flex-center">
+            此文本仅供参考！
+        </div>
+
+        <div class="banner flex-center">
+            <img class="banner1" src="@/assets/images/banner1.png" alt="">
+        </div>
+
+        <div class="enterprise-certification">
+            <div class="title flex-center">
+                企业认证
+            </div>
+            <div class="item mt-20 flex-space-around">
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+            </div>
+
+            <div class="item mt-20 flex-space-around">
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+                <div class="item_box flex-x_a-j-center">
+                    <div class="img_box flex-center">
+                        <img src="@/assets/images/icon-huawei.png" alt="">
+                    </div>
+                    <div>华为云初创伙伴</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="apply_for flex-x_a-j-center">
+            <div class="text">
+                让企业家专注自己的事业
+            </div>
+            <div class="text">
+                早早财专家即刻为您解答
+            </div>
+            <div class="btn">
+                申请免费试用
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="upper">
+                <div class="left">
+                    <div class="item">
+                        <div class="logo">
+                            早早财
+                            <!-- <img src="" alt=""> -->
+                        </div>
+                        <div class="third_party">
+                            <img src="@/assets/images/icon-wx.png" alt="">
+                            <img src="@/assets/images/icon-wx.png" alt="">
+                            <img src="@/assets/images/icon-wx.png" alt="">
+                            <img src="@/assets/images/icon-wx.png" alt="">
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="title">服务</div>
+                        <div class="mt-20">
+                            <div class="item_i">产品</div>
+                            <div class="item_i">价格</div>
+                            <div class="item_i">客户</div>
+                            <div class="item_i">活动</div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div>公司</div>
+                        <div class="mt-20">
+                            <div class="item_i">联系我们</div>
+                            <div class="item_i">隐私政策</div>
+                            <div class="item_i">服务条款</div>
+                            <div class="item_i">数据安全</div>
+                            <div class="item_i">免责条款</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="right">
+                    <div>联系我们</div>
+                    <div class="right_box mt-20">
+                        <div>
+                            地址：北京市海淀区北京理工大学国防科技园1号楼606室
+                        </div>
+                        <div>
+                            电话/微信：159-4002-3641
+                        </div>
+                        <div>
+                            Email：contact@zaozaocai.com
+                        </div>
+                        <div>
+                            添加客服微信：
+                            <div class="ewm">
+                                <img src="@/assets/images/ewm.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="below flex-center">
+                <div>Copyright ©2023 HiBug 北京一平方科技有限公司</div>
+                <div>网站备案号：京ICP备2021031205号-7</div>
+                <div class="flex-center">
+                    <img src="@/assets/images/icon-jc.png" alt="">
+                    京公网安备 11010802040993号
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+
+        }
+    },
+
 }
 </script>
 
 <style lang="scss" scoped>
+.activity {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 50px 0;
+    padding: 20px 0 0 0;
+    box-sizing: border-box;
 
+    .describe {
+        font-size: 38px;
+        width: 100%;
+        height: 200px;
+    }
+
+    .banner {
+        width: 100%;
+
+        .banner1 {
+            width: 90%;
+        }
+    }
+
+    .enterprise-certification {
+        width: 90%;
+        padding: 20px 0;
+        box-sizing: border-box;
+        border: 1px solid #000;
+        background-color: #faf7fe;
+
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+
+        .title {
+            width: 100%;
+            height: 80px;
+            font-size: 26px;
+            font-weight: 600;
+        }
+
+        .item {
+            width: 100%;
+
+            .img_box {
+                width: 80px;
+                height: 60px;
+                background-color: #fff;
+
+                img {
+                    width: 50px;
+                    height: 50px;
+                }
+
+                margin-bottom: 10px;
+            }
+
+
+        }
+    }
+
+    .apply_for {
+        width: 100%;
+        padding: 24px 0;
+        box-sizing: border-box;
+        background-color: #e6e6e6;
+
+        .text {
+            font-family: 楷体;
+            font-size: 18px;
+            margin: 5px 0;
+        }
+
+        .text:nth-child(1) {
+            font-size: 25px;
+        }
+
+        .btn {
+            padding: 10px 20px;
+            border-radius: 20px;
+            margin-top: 20px;
+            cursor: pointer;
+            background-color: #dec745;
+        }
+
+        .btn:hover {
+            background-color: #ecde8b;
+        }
+    }
+
+    .footer {
+        width: 100%;
+        background-color: #fefefe;
+        padding: 20px 0 0 0;
+        box-sizing: border-box;
+
+        .upper {
+            height: 100% !important;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0 20px;
+            box-sizing: border-box;
+            border-bottom: 1px solid #ccc;
+
+            .left {
+                width: 60%;
+                height: 100% !important;
+                display: flex;
+                align-items: top;
+                justify-content: space-between;
+                padding: 20px 0;
+                box-sizing: border-box;
+
+                .item:nth-child(1) {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+
+                    .logo {
+                        font-size: 40px;
+                    }
+
+                    .third_party {
+                        img {
+                            width: 20px;
+                            height: 20px;
+                        }
+                    }
+                }
+
+                .item:nth-child(2) {
+                    .item_i {
+                        padding: 4px 0;
+                    }
+                }
+
+                .item:nth-child(3) {
+                    .item_i {
+                        padding: 4px 0;
+                    }
+                }
+            }
+
+            .right {
+                width: 20%;
+                height: 100%;
+                padding: 20px 0;
+                box-sizing: border-box;
+
+                .right_box {
+                    div {
+                        padding: 4px 0;
+
+                        .ewm {
+                            width: 80px;
+                            height: 80px;
+
+                            img {
+                                width: 80px;
+                                height: 80px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        .below {
+            padding: 10px 0;
+            div {
+                margin: 0 10px;
+            }
+        }
+    }
+}
+
+.mt-20 {
+    margin-top: 20px;
+}
+
+.mt-10 {
+    margin-top: 10px;
+}
+
+.flex-space-around {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.flex-x_a-j-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.flex-center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 </style>
